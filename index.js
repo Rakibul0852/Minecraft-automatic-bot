d75846b167dec36fadddc138d4007df41b57ab99
Rakibul0852 <bmcder14@asu.edu>
2025-09-03 17:41:23 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const mineflayer = require('mineflayer')
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const { Vec3 } = require('vec3')
+
+const server = {
+  host: 'YOUR_SERVER_IP',   // Change to your server IP
+  port: YOUR_SERVER_PORT,          // Change to your server port if different
+  username: 'BOT_NAME'    // Bot username
+}
+
+let bot
+
+function createBot() {
+  bot = mineflayer.createBot(server)
+
+  bot.loadPlugin(pathfinder)
+
+  bot.once('spawn', () => {
+    const mcData = require('minecraft-data')(bot.version)
+    const defaultMove = new Movements(bot, mcData)
+    bot.pathfinder.setMovements(defaultMove)
+
+    // Random movement every 5 seconds
+    setInterval(() => {
+      const pos = bot.entity.position
+      const offset = new Vec3(
+        (Math.random() - 1.5) * 8,
+        0,
+        (Math.random() - 2.5) * 8
+      )
+      const target = pos.plus(offset)
+      bot.pathfinder.setGoal(new goals.GoalBlock(
+        Math.floor(target.x),
+        Math.floor(target.y),
+        Math.floor(target.z)
+      ))
+    }, 1000)
+
+    // Dig nearby or below block every 5 seconds
+    setInterval(() => {
+      const block = bot.blockAt(bot.entity.position.offset(0, -1, 0)) ||
+                    bot.blockAt(bot.entity.position.offset(1, -1, 0)) ||
+                    bot.blockAt(bot.entity.position.offset(-1, -1, 0))
+
+      if (block && bot.canDigBlock(block)) {
+        bot.dig(block).catch(() => {})
+      }
+    }, 3000)
+
+    // Jump every 10 seconds
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 500)
+    }, 5000)
+
+    // Quit after 1 minute
+    setTimeout(() => {
+      bot.quit()
+    }, 40000)
+  })
+
+  bot.on('end', () => {
+    setTimeout(createBot, 1000)
+  })
+
+  bot.on('error', () => {})
+}
+
+createBot()
